a4056610605baf253251636ff1e3b485f8266dce
usman1fhs <s.usman.fhs@gmail.com>
2025-09-30 17:20:30 +0530

    Initial commit.

---
+++ app/components/dashboard_home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const dashboard_home = () => {
+    return (
+        <div>
+            <h1 className='text-bold text-green-500 '>Dashboard Home</h1>
+        </div>
+    )
+}
+
+export default dashboard_home
